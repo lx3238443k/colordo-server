53b21e3cd288b7e75e851da3035a14c08b0578db
Richard <shenxs@users.noreply.github.com>
2018-02-06 15:20:23 +0800

    修复user新增，修复base undefine

--- app/service/todo.js
+++ app/service/todo.js
@@ -1,62 +1,62 @@
 const { Service } = require('egg');
 class TodoService extends Service {
 
-    //高阶函数，fun--函数，param--参数
-    //执行成功返回‘suc’反之返回‘fail’
-    async base(fun, param) {
-        try {
-            //如果有返回则返回，没有则返回'suc'
-            return fun.apply(null, param) || 'suc';
-        } catch (err) {
-            console.log(err);
-            return 'fail';
-        }
-    }
-
-    async msq_addTodo(openid, todo) {
-        //插入todo
-        let msq_res1 = await this.app.mysql.insert('todos', todo);
-        //建立关联
-        const usertodo = {
-            userId: openid,
-            todoId: todo.remindId,
-            isOwner: true
-        }
-        let msq_res2 = await this.app.mysql.insert('userTodo', usertodo);
-    }
-
-    async msq_deleteTodo(openid, remindId) {
-        //先删除todouser
-        let msq_res1 = await this.app.mysql.delete('todoUser', { todoId: todoid });
-        //再删除todos
-        let msq_res2 = await this.app.mysql.delete('todos', { remindId: todoid });
-    }
-
-    async msq_updateTodo(openid, remindId, row) {
-        row['remindId'] = remindId;
-        let msq_res = await this.app.mysql.update('todos', row);
-    }
-
-    //返回openid对应的所有的todos
-    async msq_getTodos(openid){
-        let todos=await this.app.mysql.query('select * from todos left join usertodo where usertodo.userId=?',[openid])
-        return todos;
-    }
-
     async addTodo(openid, todo) {
-        return base(msq_addTodo, arguments);
+        return base(this, msq_addTodo, arguments);
     }
     async deleteTodo(openid, todo) {
-        return base(msq_deleteTodo, arguments);
+        return base(this,msq_deleteTodo, arguments);
     }
     async updateTodo(openid, todo) {
-        return base(msq_updateTodo, arguments);
+        return base(this,msq_updateTodo, arguments);
     }
     async getTodo(openid) {
-        return base(msq_getTodos,arguments);
+        return base(this,msq_getTodos, arguments);
     }
+}
+
+//高阶函数，fun--函数，param--参数
+//执行成功返回‘suc’反之返回‘fail’
+async function base(context,fun, param) {
+    try {
+        //如果有返回则返回，没有则返回'suc'
+        let result=await fun.apply(context, param);
+        return result || 'suc';
+    } catch (err) {
+        console.log(err);
+        return 'fail';
+    }
+}
 
 
+async function msq_addTodo(openid, todo) {
+    //插入todo
+    let msq_res1 = await this.app.mysql.insert('todos', todo);
+    //建立关联
+    const usertodo = {
+        userId: openid,
+        todoId: todo.remindId,
+        isOwner: true
+    }
+    let msq_res2 = await this.app.mysql.insert('userTodo', usertodo);
+}
+
+async function msq_deleteTodo(openid, remindId) {
+    //先删除todouser
+    let msq_res1 = await this.app.mysql.delete('todoUser', { todoId: todoid });
+    //再删除todos
+    let msq_res2 = await this.app.mysql.delete('todos', { remindId: todoid });
+}
+
+async function msq_updateTodo(openid, remindId, row) {
+    row['remindId'] = remindId;
+    let msq_res = await this.app.mysql.update('todos', row);
+}
+
+//返回openid对应的所有的todos
+async function msq_getTodos(openid) {
+    let todos = await this.app.mysql.query('select * from todos left join usertodo where usertodo.userId=?', [openid])
+    return todos;
 }
 
 module.exports = TodoService;
--- app/service/user.js
+++ app/service/user.js
@@ -7,10 +7,9 @@ class UserService extends Service{
         const secret ='2fcb7f62ba044d0395345d67a64b94fe';
         // 调用微信接口获取openid
         const res = await this.ctx.curl(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`, { dataType: 'json' });
-        const upsert = 'REPLACE INTO `user` (`openId` ,`gender` ,`city` ,`province`,`country`,`avatarUrl`,`nickName`,`session_key`) VALUES (?,? ,?,?,?,?,?,?)';
-        const info  =[res.data.openid,userInfo.gender,userInfo.city,userInfo.province,userInfo.country,userInfo.avatarUrl,userInfo.nickName,res.data.session_key];
+        const upsert = 'INSERT INTO `user` (`openId` ,`gender` ,`city` ,`province`,`country`,`avatarUrl`,`nickName`,`session_key`) VALUES (?,? ,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE session_key=?';
+        const info  =[res.data.openid,userInfo.gender,userInfo.city,userInfo.province,userInfo.country,userInfo.avatarUrl,userInfo.nickName,res.data.session_key,res.data.session_key];
         const sql_res = await this.app.mysql.query(upsert,info); 
-     
         return res.data;
     }
   
