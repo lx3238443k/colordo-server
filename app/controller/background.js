95f99dc67c30717adf951663402e4d288a2d4d31
Richard <shenxs@users.noreply.github.com>
2018-02-09 13:56:39 +0800

    修复图片缓存引起的背景切换问题

--- app/controller/background.js
+++ app/controller/background.js
@@ -8,31 +8,41 @@ class BackgroundController extends Controller {
 
     // 上传文件,文件&openid
     async upload() {
-        const stream = await this.ctx.getFileStream();
-        // console.log(stream)
-        const filename = encodeURIComponent(stream.fieldname) + path.extname(stream.filename).toLowerCase();
-        const target = path.join(this.config.baseDir, 'app/public/pictures', filename);
-        const writeStream = fs.createWriteStream(target);
         let result;
+
+        const stream = await this.ctx.getFileStream();
+        const openid=encodeURIComponent(stream.fieldname);
+        const filename = openid + Date.parse(new Date()) + path.extname(stream.filename).toLowerCase();
+
         try {
+            const user=await this.app.mysql.get('user',{openId:openid});
+            const currentFile=user.backgroundImage;
+            const oldpath= path.join(this.config.baseDir, this.config.multipart.picturePath, currentFile);
+            if(fs.existsSync(oldpath)){
+                fs.unlinkSync(oldpath);
+            }
+            const target = path.join(this.config.baseDir, this.config.multipart.picturePath, filename);
+            const writeStream = fs.createWriteStream(target);
             await awaitWriteStream(stream.pipe(writeStream));
+            await this.app.mysql.update('user',{backgroundImage:filename},{where:{openId:openid}})
             result='suc'
         } catch (err) {
             result='fail'
             await sendToWormhole(stream);
             throw err;
         }
-
-        this.ctx.body =filename;
-
+        this.ctx.body ={result:result,filename:filename};
     }
     //下载图片，openid
     async download() {
         const filePath = path.resolve(this.config.multipart.picturePath, this.ctx.params.filename);
         this.ctx.set('Content-Type', 'image/' + path.extname(filePath).slice(1));
-
         this.ctx.body = fs.createReadStream(filePath);
     }
+}
+
+function deleteBackgroundImage(openid){
+
 }
 
 
